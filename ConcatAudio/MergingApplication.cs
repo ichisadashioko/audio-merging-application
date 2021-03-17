51824554fd2b6db3d11aaf7030b7bc99bba7588e
shioko <ichisadashioko@gmail.com>
2021-03-17 13:42:39 +0000

    Add project files.

---
+++ ConcatAudio/MergingApplication.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ConcatAudio
+{
+    public partial class MergingApplication : Form
+    {
+        public MergingApplication()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int size = 1;
+            var fileDialog = new OpenFileDialog();
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileToOpen = fileDialog.FileName;
+                audioFileEntriesListBox.Items.Add(fileToOpen);
+
+                //FileInfo selectedFile = new FileInfo(fileToOpen);
+            }
+        }
+    }
+}
