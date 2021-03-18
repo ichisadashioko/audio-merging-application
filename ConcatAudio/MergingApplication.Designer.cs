f7087ee43e3ad4171f08668f79dc834a17d18dc6
shioko <ichisadashioko@gmail.com>
2021-03-18 06:57:41 +0000

    refactor form

--- ConcatAudio/MergingApplication.Designer.cs
+++ ConcatAudio/MergingApplication.Designer.cs
@@ -43,135 +43,135 @@ namespace ConcatAudio
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            //
+            // 
             // button1
-            //
+            // 
             this.button1.Location = new System.Drawing.Point(13, 13);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(160, 48);
+            this.button1.Size = new System.Drawing.Size(345, 48);
             this.button1.TabIndex = 0;
             this.button1.Text = "Browser source file";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            //
+            // 
             // audioFileEntriesListBox
-            //
+            // 
             this.audioFileEntriesListBox.FormattingEnabled = true;
             this.audioFileEntriesListBox.Location = new System.Drawing.Point(13, 68);
             this.audioFileEntriesListBox.Name = "audioFileEntriesListBox";
-            this.audioFileEntriesListBox.Size = new System.Drawing.Size(160, 368);
+            this.audioFileEntriesListBox.Size = new System.Drawing.Size(345, 368);
             this.audioFileEntriesListBox.TabIndex = 1;
-            //
+            // 
             // label1
-            //
+            // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(179, 13);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(364, 16);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(140, 24);
+            this.label1.Size = new System.Drawing.Size(82, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Target Location";
-            //
+            // 
             // label2
-            //
+            // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(179, 37);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(364, 43);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(156, 24);
+            this.label2.Size = new System.Drawing.Size(88, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "Target File Name";
-            //
+            // 
             // textBox1
-            //
-            this.textBox1.Location = new System.Drawing.Point(395, 13);
+            // 
+            this.textBox1.Location = new System.Drawing.Point(488, 13);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(393, 20);
+            this.textBox1.Size = new System.Drawing.Size(300, 20);
             this.textBox1.TabIndex = 4;
-            //
+            // 
             // textBox2
-            //
-            this.textBox2.Location = new System.Drawing.Point(395, 40);
+            // 
+            this.textBox2.Location = new System.Drawing.Point(488, 40);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(392, 20);
+            this.textBox2.Size = new System.Drawing.Size(299, 20);
             this.textBox2.TabIndex = 5;
-            //
+            // 
             // label3
-            //
+            // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(183, 68);
+            this.label3.Location = new System.Drawing.Point(364, 70);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(73, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "Target Format";
-            //
+            // 
             // label4
-            //
+            // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(186, 85);
+            this.label4.Location = new System.Drawing.Point(364, 97);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(48, 13);
             this.label4.TabIndex = 7;
             this.label4.Text = "Progress";
-            //
+            // 
             // progressBar1
-            //
-            this.progressBar1.Location = new System.Drawing.Point(183, 102);
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(367, 125);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(604, 23);
+            this.progressBar1.Size = new System.Drawing.Size(420, 23);
             this.progressBar1.TabIndex = 8;
-            //
+            // 
             // radioButton1
-            //
+            // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(395, 66);
+            this.radioButton1.Location = new System.Drawing.Point(488, 68);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(47, 17);
             this.radioButton1.TabIndex = 9;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "MP3";
             this.radioButton1.UseVisualStyleBackColor = true;
-            //
+            // 
             // radioButton2
-            //
+            // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(577, 66);
+            this.radioButton2.Location = new System.Drawing.Point(736, 68);
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(50, 17);
             this.radioButton2.TabIndex = 10;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "WAV";
             this.radioButton2.UseVisualStyleBackColor = true;
-            //
+            // 
             // button2
-            //
-            this.button2.Location = new System.Drawing.Point(180, 132);
+            // 
+            this.button2.Location = new System.Drawing.Point(367, 165);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 11;
             this.button2.Text = "Play Test";
             this.button2.UseVisualStyleBackColor = true;
-            //
+            // 
             // button3
-            //
-            this.button3.Location = new System.Drawing.Point(395, 132);
+            // 
+            this.button3.Location = new System.Drawing.Point(547, 165);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 12;
             this.button3.Text = "Cancel";
             this.button3.UseVisualStyleBackColor = true;
-            //
+            // 
             // button4
-            //
-            this.button4.Location = new System.Drawing.Point(711, 131);
+            // 
+            this.button4.Location = new System.Drawing.Point(711, 165);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 13;
             this.button4.Text = "Finish";
             this.button4.UseVisualStyleBackColor = true;
-            //
+            // 
             // MergingApplication
-            //
+            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
